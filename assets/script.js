a602682f244050053082788407dd4369365e9f21
zjalic <markozjalic@gmail.com>
2021-06-26 01:10:13 +0200

    Added skills, tech and this

--- assets/script.js
+++ assets/script.js
@@ -60,6 +60,8 @@ function addTerminalOutput(command){
         "<b>about</b> -  More info about me<br>" +
         "<b>exp</b> - Info about my work experience<br>" +
         "<b>education</b> - Info about my education<br>" +
+        "<b>tech</b> - Technologies that i use<br>" +
+        "<b>skills</b> - Brief overview of my skills <br>" +
         "<b>cv</b> - View my cv in pdf format<br>" +
         "<b>contact</b> - Contact (Email,Github,LinkedIn)<br>" +
         "<b>clear</b> - Clear terminal/command line<br>" +
@@ -104,7 +106,8 @@ function addTerminalOutput(command){
         "Hi, I am Marko, I am from Belgrade, Serbia. I graduated Computer Science and currently working as Software Developer in Exela.<br>"+
         "Before Exela I was part of the Engineering Ingegneria Informatica Spa group, as Junior Soultion Developer, Invej as System and Database Administrator. <br>" +
         "and as part of internship I was Student Assistent and IT Technician at Singidunum University. <br><br>" +
-        "... " +
+        "I like to create mini apps, if you need some kind of app and it will not take too much time please contact me ... <br>"+
+        "Also i enjoy in games, movies, music, food, cars, guns... Like the most man I guess... " +
         "</div>");
         return;
     }
@@ -115,6 +118,45 @@ function addTerminalOutput(command){
         "<a href = \"mailto: markozjalic@gmail.com\">markozjalic@gmail.com</a><br><br>" +
         "<a href = \"https://www.linkedin.com/in/zjalic\" target=\"_blank\" rel=\"noopener noreferrer\">LinkedIn</a><br><br>" +
         "<a href = \"https://github.com/zjalic\" target=\"_blank\" rel=\"noopener noreferrer\">GitHub</a><br>" +
+        "</div>");
+        return;
+    }
+
+    if(command == "tech"){
+        document.getElementById("terminal-body").insertAdjacentHTML("beforeend",
+        "<div class=\"terminal-output\">" +
+        "<b>Technologies that I am using depend mostly what I am trying to build.<br>"+
+        "Some i know excellent and some i know enough to use it </b><br><br>" +
+        spaces(4)+"Desktop App"       +spaces(6)+ " Java Swing <br>"+
+        spaces(4)+"Small Web App"     +spaces(4)+ " PHP, JavaScript, MariaDB <br>"+
+        spaces(4)+"Business Web App"  +spaces(1)+ " Java Spring w/ Liquibase and JSP w/ Tiles, Oracle<br>"+
+        spaces(4)+"Bots & Scripts"    +spaces(3)+ " Python, SQLite <br>"+
+        spaces(4)+"Mobile"            +spaces(11)+" Flutter <br><br>" +
+        "<b>Tools that helps me</b><br><br>" +
+        spaces(4)+"IDE"             +spaces(14)+ " Eclipse & Netbeans <br>" +
+        spaces(4)+"Editor"          +spaces(11)+ " VSCode <br>" +
+        spaces(4)+"Version Control" +spaces(2) + " Git & SVN <br>" +
+        spaces(4)+"Misc"            +spaces(13)+ " Docker, Maven, Tomcat <br>" +
+        "</div>");
+        return;
+    }
+
+    if(command == "skills"){
+        document.getElementById("terminal-body").insertAdjacentHTML("beforeend",
+        "<div class=\"terminal-output skills\">" +
+        "<b>Programming</b> is my current job, i like it but personal projects are more interesting<br>"+
+        "<b>Networking and System Administration</b> is something that i know pretty decent <br>" +
+        "<b>Cryptography and Security</b> is something that i really like but there is plenty space for improvement <br>" +
+        "My native is <b>Serbian</b>, but I am speaking also <b>English</b> it's not perfect but okay and a little bit <b>Italian</b><br>" +
+        "<b>Problem solving</b> is my best attribute. I am <b>Pragmatic</b> and <b>Idealistic</b> ...<br>" +
+        "</div>");
+        return;
+    }
+
+    if(command == "this"){
+        document.getElementById("terminal-body").insertAdjacentHTML("beforeend",
+        "<div class=\"terminal-output\">" +
+        "Builded from scratch using JQuery and StackOverFlow :D"+
         "</div>");
         return;
     }
